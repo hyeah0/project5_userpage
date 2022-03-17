40451d77e49752402622c21377cd2fd52d1da1d5
hye <hye0ah0@gmail.com>
2022-03-17 17:13:20 +0900

    first commit

---
+++ js/login.js
@@ -0,0 +1,34 @@
+const wellcomPage = document.querySelector(".wellcom-page");
+const wellcomPageForm = document.querySelector(".wellcom-page__form");
+const wellcomPageGreeting = document.querySelector(".wellcom-page__h1");
+const userName = document.querySelector("#wellcom-page__username");
+
+const HIDDEN = "hidden"
+const USERNAME = "username"
+
+function loginSubmit(event){
+    event.preventDefault();
+    const username = userName.value;
+    localStorage.setItem(USERNAME, username);
+    greeting(username);
+}
+
+function greeting(username){
+    wellcomPageForm.classList.add(HIDDEN);
+    wellcomPageGreeting.classList.remove(HIDDEN);
+    wellcomPageGreeting.innerHTML = `HELLO ${username} </br> VON VOYAGE`
+}
+
+const savedUserName = localStorage.getItem(USERNAME);
+
+if(savedUserName === null){
+    wellcomPageForm.classList.remove(HIDDEN);
+    wellcomPageForm.addEventListener("submit",loginSubmit);
+}
+else{
+    greeting(savedUserName);
+}
+
+
+
+
